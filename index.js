0e506584d145cc49f9dc13aa71bc4f570c4ffba6
mr9 asif <asifalibd002@gmail.com>
2024-05-13 22:34:32 +0600

    update viewdetails

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('dotenv').config()
 const cors = require('cors');
+var cookieParser = require('cookie-parser')
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 4000;
 
@@ -12,8 +14,31 @@ app.use(cors({
     credentials: true 
   }));
 app.use(express.json());
+app.use(cookieParser())
 
 
+// own middle ware
+const logger = async(req, res, next)=>{
+  console.log('called', req.host, req.originalUrl)
+  next()
+}
+
+const VarifyToken = async(req, res, next)=>{
+const token = req.cookies?.Token
+console.log('tooken', token)
+if(!token){
+ return res.status(401).send({messege:'unauthorized'})
+}
+jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded)=>{
+  if(err){
+    console.log(err)
+    return res.status(401).send({messege:"forbidden"})
+  }
+  console.log('token info', decoded)
+  req.user = decoded
+  next()
+})
+}
 
 // modngo start--
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p7hqbnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -26,19 +51,56 @@ const client = new MongoClient(uri, {
     }
   });
 
+  const cookieOption = {
+    httpOnly:true,
+   secure:process.env.NODE_ENV === 'production' ? true : false,
+   sameSite:process.env.NODE_ENV === "production"? "none" : "strict"
+}
 
   async function run() {
     try {
     //    Collection
       const BlogsCollection = client.db('BlogsDB').collection('Blogs');
       const CommentCollection = client.db('BlogsDB').collection('Comments')
+      const WishListCollection = client.db('BlogsDB').collection('wishLists')
+
+    //  Auth reletad
+    app.post('/jwt',  async(req, res)=>{
+      const user = req.body;
+        console.log('user', user)
+        const token = jwt.sign(user,  process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+
+        res
+        .cookie(' Token', token , cookieOption)
+        .send({success: 'true'})
+   })
+
+     //  log out
+     app.post('/logout', async(req, res)=>{
+      
+      res
+      .clearCookie('Token', { expires:new Date(0), ...cookieOption})
+      .send({success:true})
+      
+   })
+
+
 //  post-----
-      app.post('/addblogs', async(req,res)=>{
+      app.post('/addblogs',  async(req,res)=>{
          const query= req.body;
          const result = await BlogsCollection.insertOne(query);
          console.log(result)
          res.send(result)
       })
+
+      app.post('/addwishlist',  async(req,res)=>{
+         const query= req.body;
+         console.log(query)
+         const result = await WishListCollection.insertOne(query);
+         console.log(result)
+         res.send(result)
+      })
+
       app.post('/addcomments', async(req,res)=>{
          const query= req.body;
          const result = await CommentCollection.insertOne(query);
@@ -71,11 +133,26 @@ const client = new MongoClient(uri, {
          res.send(result)
          console.log(result)
       })
+
+      // delete
+      app.delete('/wish/:id', async(req, res)=>{
+         const id = req.params.id;
+          console.log(id)
+         const query = {_id: id};
+         console.log(query)
+         const result = await WishListCollection.deleteOne(query);
+         console.log(result)
+         res.send(result)
+      })
             
     //   get-----
      app.get('/allblogs', async(req, res)=>{
          const result = await BlogsCollection.find().toArray();
-        
+        //  const queryuser= req.query.email;
+        //  console.log('user' , queryuser)
+        //  if(req.user.email !== req.query.email){
+        //    return res.status(401).send({messege:"not authorized"})
+        //  }
          res.send(result)
      })
      
@@ -90,8 +167,34 @@ const client = new MongoClient(uri, {
         const query = {_id : new ObjectId(id)};
         
          const result = await BlogsCollection.findOne(query);
-         console.log(result)
+       
+       
          res.send(result)
+     })
+
+     app.get('/wishlistblogs/:email',VarifyToken, async(req,res)=>{
+          const userEmail = req.params.email;
+          const query = {email : userEmail};
+          const result = await WishListCollection.find(query).toArray();
+             const queryuser= req.user.email;
+           console.log('user' , queryuser)
+          //  if(req.user.email !== req.user.email){
+          //   return res.status(401).send({messege:"not authorized"})
+          // }
+          res.send(result)
+
+     })
+     app.get('/wishlist/:email', async(req,res)=>{
+          const userEmail = req.params.email;
+          const query = {email : userEmail};
+          const result = await WishListCollection.find(query).toArray();
+          //    const queryuser= req.user.email;
+          //  console.log('user' , queryuser)
+          //  if(req.user.email !== req.user.email){
+          //   return res.status(401).send({messege:"not authorized"})
+          // }
+          res.send(result)
+
      })
 
       // Send a ping to confirm a successful connection
